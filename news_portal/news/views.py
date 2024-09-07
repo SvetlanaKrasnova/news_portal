75ab35a7be7b322d2ec5bb4747274e9298dfd2a5
Svetlana Krasnova <venerliline@yandex.ru>
2024-09-07 21:17:00 +0300

    adding url news

--- news_portal/news/views.py
+++ news_portal/news/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+class PostList(ListView):
+    """
+    Информация о всех публикациях
+    """
+    model = Post
+    ordering = 'title'
+    template_name = 'all_news.html'
+    context_object_name = 'all_news'
+
+class PostDetail(DetailView):
+    """
+    Показывает 1 статью
+    """
+    model = Post
+    template_name = 'one_news.html'
+    context_object_name = 'one_news'
