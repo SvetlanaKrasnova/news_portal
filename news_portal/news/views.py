e5f9c9a2dd8e2c856aba0c4ef25d1ff6c57bb5f4
Svetlana Krasnova <venerliline@yandex.ru>
2024-09-11 20:00:36 +0300

    refactoring format templates

--- news_portal/news/views.py
+++ news_portal/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
 
@@ -8,9 +8,14 @@ class PostList(ListView):
     Информация о всех публикациях
     """
     model = Post
-    ordering = 'title'
+    ordering = '-publishing_date'
     template_name = 'all_news.html'
     context_object_name = 'all_news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
 
 class PostDetail(DetailView):
     """
@@ -18,4 +23,9 @@ class PostDetail(DetailView):
     """
     model = Post
     template_name = 'one_news.html'
-    context_object_name = 'one_news'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
