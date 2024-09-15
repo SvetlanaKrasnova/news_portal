7f8eea737e98977ef105c8c63bf75179aa6262a8
Svetlana Krasnova <venerliline@yandex.ru>
2024-09-15 13:54:18 +0300

    adding Pagination

--- news_portal/news/views.py
+++ news_portal/news/views.py
@@ -11,6 +11,7 @@ class PostList(ListView):
     ordering = '-publishing_date'
     template_name = 'all_news.html'
     context_object_name = 'all_news'
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
