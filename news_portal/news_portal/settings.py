4db0c9292274eaaf22e810d500ab3f05e7f92c73
Svetlana Krasnova <venerliline@yandex.ru>
2024-09-27 21:09:46 +0300

    adding registration using yandex

--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     # ... include the providers you want to enable:
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.yandex',
 ]
 
 SITE_ID = 1
