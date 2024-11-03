f30c3d4de452b8a0997ff03f6c9185aaa01b34f6
Svetlana Krasnova <venerliline@yandex.ru>
2024-11-03 17:09:50 +0300

    eddit admin panel

--- news_portal/fpages/admin.py
+++ news_portal/fpages/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
+from news.models import Category, Post
 
 
 # Define a new FlatPageAdmin
@@ -19,6 +20,21 @@ class FlatPageAdmin(FlatPageAdmin):
     )
 
 
+def delete_post(modeladmin, request, queryset):
+    # queryset.delete(id=request.id)
+    # delete_post.short_description = 'Удаление публикации'
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Post._meta.get_fields()]
+    list_filter = ('title', 'category', 'type_post', 'author', 'rating')
+    search_fields = ('title', 'category__name', 'type_post', 'author__full_name')
+    actions = [delete_post]
+
+
 # Re-register FlatPageAdmin
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
+admin.site.register(Category)
+admin.site.register(Post)
